b85e7976cb6110de1212a279a7a002771e8f3d07
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2014-04-28 18:25:34 +0900

    Non useful configs, commented out.

--- conf/perl-completion.el
+++ conf/perl-completion.el
@@ -1,5 +1,5 @@
-;;; (add-hook 'cperl-mode-hook
-;;;           '(lambda ()
-;;;              (require 'perl-completion)
-;;;              (perl-completion-mode t)
-;;;              (add-to-list 'ac-sources 'ac-source-perl-completion)))
+(add-hook 'cperl-mode-hook
+          '(lambda ()
+             (require 'perl-completion)
+             (perl-completion-mode t)
+             (add-to-list 'ac-sources 'ac-source-perl-completion)))
