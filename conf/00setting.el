3a1045a033458426660c456f0ade4548e113533d
OGATA Tetsuji <tetsuji.ogata@gmail.com>
2014-08-04 14:48:18 +0900

    modify format.

--- conf/00setting.el
+++ conf/00setting.el
@@ -17,12 +17,13 @@
 (setq require-final-newline t) ;; Always end a file with a newline
 (setq next-line-add-newlines nil) ;; Stop at the end of the file, not just add lines
 (setq scroll-conservatively 1) ;; 画面端でカーソルを一行だけスクロール
-
-(add-hook 'after-save-hook
-          'executable-make-buffer-file-executable-if-script-p) ;; chmod +x
 (setq kill-ring-max 10)
 
-(setq completion-ignore-case t) ;; 大文字小文字同一視補完 http://d.hatena.ne.jp/hirose31/20130304/1362372183
+;; chmod +x
+(add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+;; 大文字小文字同一視補完 http://d.hatena.ne.jp/hirose31/20130304/1362372183
+(setq completion-ignore-case t)
 
 
 ;; TODO: misc.el や my.el と重複している設定があるかもしれないので整理
